72e5c0720744a0c053519170cab99f826cd1f949
Fadhli Mulyana <baba.fadhli@gmail.com>
2023-04-21 06:10:53 +0700

    feat: add s3 storage

--- apps/book/models.py
+++ apps/book/models.py
@@ -1,6 +1,13 @@
+import os
 from django.db import models
+
+
+
+def get_upload_path(instance, filename):
+    return os.path.join("book", "%d" % instance.id, filename)
 
 # Create your models here.
 class Book(models.Model):
     name = models.CharField()
     description = models.CharField()
+    cover = models.ImageField(null=True, blank=True, upload_to=get_upload_path)
--- core/settings.py
+++ core/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'django_filters',
+    'storages',
     'apps.book',
 ]
 
@@ -132,15 +133,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
-
-MEDIA_ROOT = 'media'
-MEDIA_URL = 'media/'
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -204,4 +196,34 @@ sentry_sdk.init(
     traces_sample_rate=1.0
 )
 
-STORAGES = {"default": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"}}
+USE_S3 = os.getenv('USE_S3') == 'TRUE'
+
+if USE_S3:
+    STORAGES = {
+        "default": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"},
+        "staticfiles": {"BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage"}
+    }
+    # aws settings
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    # AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_ENDPOINT_URL = os.getenv('AWS_S3_ENDPOINT_URL')
+    # AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    # AWS_LOCATION = 'static'
+    # STATIC_URL = f'{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
+    # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# else:
+    # STATIC_URL = 'static/'
+    # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = 'static/'
+
+MEDIA_ROOT = 'media'
+MEDIA_URL = 'media/'
